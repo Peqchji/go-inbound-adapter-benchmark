940f3e287a1bfde5ad370d52d243b796f9b38e5a
Peqchji <jirapat3774@gmail.com>
2026-02-03 01:24:48 +0700

    feat: add Inmemory client

--- internal/client/database/inmemory/client.go
+++ internal/client/database/inmemory/client.go
@@ -1,1 +1,22 @@
 package inmemory
+
+
+type InMemoryClient struct {
+	db *InMemoryDB
+}
+
+func NewInMemoryClient() *InMemoryClient {
+	return &InMemoryClient{
+		db: NewInMemoryDB(),
+	}
+}
+
+func (c *InMemoryClient) CreateTable(name string) error {
+	newTable := NewInMemoryDBTable(name)
+
+	return c.db.AddTable(newTable)
+}
+
+func (c *InMemoryClient) GetTable(name string) (IInMemoryDBTable, error) {
+	return c.db.GetTable(name)
+}	
