9dbaf585473732608e9b27fb03b6039a23a3aee8
Peqchji <jirapat3774@gmail.com>
2026-02-03 16:35:51 +0700

    feat: containerize servers and add benchmark to docker-compose

--- start_all.ps1
+++
@@ -1,7 +0,0 @@
-$env:PORT=8080
-Start-Process -NoNewWindow -FilePath "go" -ArgumentList "run", "cmd/httpserver/main.go"
-$env:PORT=8081
-Start-Process -NoNewWindow -FilePath "go" -ArgumentList "run", "cmd/gqlserver/main.go"
-$env:GRPC_PORT=8082
-Start-Process -NoNewWindow -FilePath "go" -ArgumentList "run", "cmd/grpcserver/main.go"
-Write-Host "All servers started!"
